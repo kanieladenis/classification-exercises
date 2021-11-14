404fd9d6ec7d64daca247a46a783e5cd1515b16a
kanieladenis <kaniela.denis@gmail.com>
2021-11-14 15:07:29 -0600

    added prepare.py, prep_lesson, and excel exercises. Updated acquire.py and classification exercises for prep work.

--- acquire.py
+++ acquire.py
@@ -1,56 +1,74 @@
+import pandas as pd
+import os
+
+
+
 def get_connection(db_name):
     from env import host, user, password
     return f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
+
     
-    
-    
-def get_titanic_data():
-    import os
-    file_name = 'titanic.csv'
-    if os.path.isfile(file_name): # checks if file exist
-        df = pd.read_csv(file_name) # assigns df to read on return
-    else:
-        sql = '''
+
+def new_titanic_date():
+    sql = '''
         'SELECT * FROM passengers'
         '''
-        url = get_connection('titanic_db')
-        df = pd.read_sql(sql, url) #creates df
+    url = get_connection('titanic_db')
+    df = pd.read_sql(sql, url)
+    return df
+
+        
+    
+def get_titanic_data():
+    file_name = 'titanic.csv'
+    if os.path.isfile(file_name): # checks if file exist
+        df = pd.read_csv(file_name, index_col=0) # assigns df to read on return
+    else:
+        df = new_titanic_data() # pull data using function
         df.to_csv(file_name) # converts df to csv file
     return df
 
 
 
-def get_iris_data():
-    import os
-    file_name = 'iris.csv'
-    if os.path.isfile(file_name):
-        df = pd.read_csv(file_name)
-    else:
-        sql = '''
+def new_iris_data():
+    sql = '''
         SELECT * FROM species 
         JOIN measurements USING (species_id)
         '''
-        url = get_connection('iris_db')
-        df = pd.read_sql(sql,url)
+    url = get_connection('iris_db')
+    df = pd.read_sql(sql,url)
+    return df
+
+
+
+def get_iris_data():
+    file_name = 'iris.csv'
+    if os.path.isfile(file_name):
+        df = pd.read_csv(file_name, index_col=0)
+    else:
+        df = new_iris_data()
         df.to_csv(file_name)
     return df
 
 
 
-def get_telco_data():
-    import os
-    file_name = 'telco.csv'
-    if os.path.isfile(file_name):
-        df = pd.read_csv(file_name)
-    else:
-        sql = '''
+def new_telco_data():
+    sql = '''
         SELECT * FROM customers c
         JOIN contract_types ct ON ct.contract_type_id=c.contract_type_id
         JOIN internet_service_types ist ON ist.internet_service_type_id=c.internet_service_type_id
         JOIN payment_types pt ON pt.payment_type_id=c.payment_type_id
         '''
-        url = get_connection('telco_churn')
-        df = pd.read_sql(sql, url)
+    url = get_connection('telco_churn')
+    df = pd.read_sql(sql, url)
+
+
+
+def get_telco_data():
+    file_name = 'telco.csv'
+    if os.path.isfile(file_name):
+        df = pd.read_csv(file_name, index_col=0)
+    else:
+        df = get_telco_data()
         df.to_csv(file_name)
     return df
-
---
+++ prepare.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import acquire
+
+def prep_iris():
+    df = acquire.get_iris_data()
+    df = df.drop(columns=['species_id','measurement_id'])
+    df = df.rename(columns={'species_name':'species'})
+    df_species = pd.get_dummies(df.species, prefix='is')
+    df = pd.concat([df, df_species], axis=1)
+    return df
+
+
+
+def prep_titanic():
+    df = acquire.get_titanic_data()
+    drop_col = ['embarked','fare', 'class', 'deck', 'age']
+    df = df.drop(columns = drop_col)
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+    dummies_df = pd.get_dummies(df, columns = ['sex', 'embark_town'], drop_first=[True,True])
+    df = pd.concat([df,dummies_df], axis=1)
+    return df
+
+
+
+def prep_telco():
+    df = acquire.get_telco_data()
+    # id cols to drop
+    drop_cols = ['internet_service_type_id',
+             'internet_service_type_id.1',
+             'contract_type_id',
+             'contract_type_id.1',
+             'payment_type_id',
+             'payment_type_id.1']
+    # drop columns
+    df = df.drop(columns=drop_cols)
+    # replace blank spaces with zeros
+    df['total_charges'] = df.total_charges.replace(' ', '0')
+    # convert total charges from object to float
+    df.total_charges = df.total_charges.astype(float)
+    # id columns for dummy variables
+    dummies_cols = [
+ 'gender',
+ 'senior_citizen',
+ 'partner',
+ 'dependents',
+ 'phone_service',
+ 'multiple_lines',
+ 'online_security',
+ 'online_backup',
+ 'device_protection',
+ 'tech_support',
+ 'streaming_tv',
+ 'streaming_movies',
+ 'paperless_billing',
+ 'churn',
+ 'contract_type',
+ 'internet_service_type',
+ 'payment_type']
+    # creaet dummy variables and assign to variable
+    dummies_df = pd.get_dummies(df[dummies_cols], drop_first=True)
+    # concat df with dummy variable columns
+    df = pd.concat([df, dummies_df], axis=1)
+    return df
+
+
+
+def split_titanic(df):
+    train_validate, test = train_test_split(df, 
+                                             test_size=.2, 
+                                             random_state=123, 
+                                             stratify=df.survived)
+    train, validate = train_test_split(train_validate,
+                                      test_size=.3,
+                                      random_state=123,
+                                      stratify=train_validate.survived)
+    return train, validate, test
